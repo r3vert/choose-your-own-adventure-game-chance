b12d622156b736131046be3343f7630756265f81
r3vert <keenanhomsi@gmail.com>
2020-12-09 19:16:19 +0000

    Made debug file. Deleted old code

---
+++ debug.py
@@ -0,0 +1,18 @@
+from time import sleep
+from os import system, name 
+#get persons name
+pname = input('What is your name?\n')
+
+cname = input('What is your player name going to be.  \b')
+
+#This is shortened to make it easier to debug
+   
+
+print('where do you want to go east, west, north, or south.' )
+do = input()
+if do == 'north':
+  print("As you walk down the hall you meet a large mutated rat. it eats you and you die horribly")
+  
+
+
+
--- remade.py
+++ remade.py
@@ -1,4 +1,3 @@
-#
 from time import sleep
 from os import system, name 
 #get persons name
