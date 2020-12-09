74c6221f5e08d7ed5b0f319ca0869c718b289e1c
r3vert <keenanhomsi@gmail.com>
2020-12-09 18:48:28 +0000

    Initial commit

---
+++ chancescode.py
@@ -0,0 +1,75 @@
+name = input('What is your name?\n')
+print('Hi, %s.' % name)
+
+for i in range(1):
+  print('I am a bot whith self conchince, %s ...' % name )
+  print('Im going to tell you a story')
+okboomer = input('What is your player name going to be. \b')
+print('I shall call you %s for now on' % okboomer)
+print('The story shall start in a dark labrith.' ) 
+print('I hope you like it there, %s...' % okboomer)
+print('whedo re you whant to go east, west, north, or south.' )
+do = input()
+
+if do == 'north':
+  print('ok then')
+ 
+print('where do you want to go east, west, north, or south.') 
+if do == 'north' :
+  print('ok then')
+
+if do == 'south':
+  print('ok then')
+
+if do == 'west':
+  print('ok then')
+
+if do == 'east':
+  print('ok then')
+
+if do == 'south':
+  print('ok then')
+print('where do you whant to go east, west, north, or south.'  ) 
+if do == 'north' :
+  print('ok then')
+
+if do == 'south':
+  print('ok then')
+
+if do == 'west':
+  print('ok then')
+
+if do == 'east':
+  print('ok then')
+
+if do == 'west':
+  print('ok then')
+
+print('whedo re you whant to go east, west, north, or south.'  ) 
+if do == 'north' :
+  print('ok then')
+
+if do == 'south':
+  print('ok then')
+
+if do == 'west':
+  print('ok then')
+
+if do == 'east':
+  print('ok then')
+
+print('whedo re you whant to go east, west, north, or south.'  ) = input()
+if do == 'north' :
+  print('ok then')
+
+if do == 'south':
+  print('ok then')
+
+if do == 'west':
+  print('ok then')
+
+if do == 'east':
+  print('ok then')
+
+if do == 'east':
+  print('ok then')
