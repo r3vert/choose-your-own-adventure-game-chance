e4b007f1e2d28aa45082d31bd5d99118f51ef6f9
r3vert <keenanhomsi@gmail.com>
2020-12-11 22:46:29 +0000

    started on 10 sided system

--- extras/testrandom.py
+++ extras/testrandom.py
@@ -1,10 +1,4 @@
 from random import randint
 one = 1
-value = randint(1, 2)
+value = randint(0, 9)
 print(value)
-do = input()
-if do == 'north':
-  if value == one:
-    print("you died")
-  else:
-    print("you survived")
