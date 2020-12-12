153000f9abb460d81031e08d9612fdb03d76cce8
r3vert <68307669+r3vert@users.noreply.github.com>
2020-12-12 15:50:28 +0000

    added more to 10 sided system

--- 10-side-system.py
+++ 10-side-system.py
@@ -1,5 +1,9 @@
 import sys
 from random import randint
-one = 1
 value = randint(1, 2)
 if value == 0 or value == 1 or value == 2 or value == 3:
+    print("nothing happened")
+elif value == 4 or value == 5 or value == 6 or value == 7 or value == 8:
+    print("you encountered a monster")
+else:
+    print()
