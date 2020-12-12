ea4d699deaf2e531363e9fdb0f5dcac08f4564ff
r3vert <68307669+r3vert@users.noreply.github.com>
2020-12-12 18:52:32 +0000

    finished 10 sided system

--- 10-side-system.py
+++ 10-side-system.py
@@ -1,9 +1,9 @@
 import sys
 from random import randint
-value = randint(1, 2)
-if value == 0 or value == 1 or value == 2 or value == 3:
+encouter = randint(0, 9)
+if encouter == 0 or encouter == 1 or encouter == 2 or encouter == 3:
     print("nothing happened")
-elif value == 4 or value == 5 or value == 6 or value == 7 or value == 8:
+elif encouter == 4 or encouter == 5 or encouter == 6 or encouter == 7 or encouter == 8:
     print("you encountered a monster")
-else:
-    print()
+elif encouter == 9:
+    print("You escaped")
