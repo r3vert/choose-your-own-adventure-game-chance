1a18d33452e94c08d27ac3300eae6af7920dcf07
r3vert <keenanhomsi@gmail.com>
2021-01-14 14:16:56 -0500

    added new line to restart.py

--- minorutil/restart.py
+++ minorutil/restart.py
@@ -1,7 +1,7 @@
 from os import name,system
 import sys
 def restartprog():
-  tryagain = input('do you want to try again')
+  tryagain = input('\n do you want to try again')
   if tryagain == 'y' or tryagain == 'yes':
   # for windows
     if name == 'nt':
